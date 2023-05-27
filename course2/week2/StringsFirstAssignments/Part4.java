2431c2919a13e777704586b308fed51e3161203f
Billuri varun <84211999+varunbiluri@users.noreply.github.com>
2023-05-27 20:45:36 +0530

    searching for specific start and stop codons

---
+++ course2/week2/StringsFirstAssignments/Part4.java
@@ -0,0 +1,27 @@
+import edu.duke.*;
+
+/**
+ * Resources: https://www.dukelearntoprogram.com//course2/index.php
+ *
+* @author billurivarun
+ * @version April 21, 2023
+ */
+
+public class Part4 {
+    public static void youtubeLinks() {
+        URLResource dukeurl = new URLResource("https://www.dukelearntoprogram.com//course2/data/manylinks.html");
+        for (String word : dukeurl.lines()) {
+            int line = word.toLowerCase().indexOf("youtube.com");
+            if (line != -1) {
+                int quotestart = word.lastIndexOf("\"", line);
+                int quotestop = word.indexOf("\"", line + 1);
+                System.out.println(word.substring(quotestart + 1, quotestop));
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Part4 urls = new Part4();
+        urls.youtubeLinks();
+    }
+}
